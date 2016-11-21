36a6631f7306fa235960194038c3e3f5099cd763
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-20 16:33:34 -0800

    fix type of app name

--- javascript/chirpApp.js
+++ javascript/chirpApp.js
@@ -1,6 +1,6 @@
 // Angular app to control all the functionality of the app's front end
 // chirpApp.js
-var app = angular.module('chripApp', []);
+var app = angular.module('chirpApp', []);
 
 app.controller('mainController', function($scope){
 
