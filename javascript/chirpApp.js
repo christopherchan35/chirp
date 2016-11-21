1601fbcd6599fade32a7380bc8c08fe4007d70f6
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-21 09:30:58 -0800

    add authController

--- javascript/chirpApp.js
+++ javascript/chirpApp.js
@@ -14,3 +14,22 @@ app.controller('mainController', function($scope){
     $scope.newpost = {created_by: '', text: '', created_at: ''};
   };
 });
+
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  postService.getAll().success(function(data){
+    $scope.posts = data;
+  });
+
+  $scope.login = function(){
+    // needs auth implementation
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    // needs auth implementation
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  }
+})
