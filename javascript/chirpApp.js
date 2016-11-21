438b97196f35a09e4c77b94f1268b15471d9dd2e
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-20 19:43:05 -0800

    add comments about binding and create post function

--- javascript/chirpApp.js
+++ javascript/chirpApp.js
@@ -2,6 +2,15 @@
 // chirpApp.js
 var app = angular.module('chirpApp', []);
 
+// One way data-binding is good for displaying, while two way data-binding
+// is good for user input
 app.controller('mainController', function($scope){
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
 
+  $scope.post = function(){
+    $scope.newPost.created_at = DateTime.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newpost = {created_by: '', text: '', created_at: ''};
+  };
 });
