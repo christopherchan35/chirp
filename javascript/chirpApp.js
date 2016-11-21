28c88b1ba41e5bd1034ed9e4dec5e1a6efddd754
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-20 16:23:21 -0800

    move controller into JS folder

---
+++ javascript/chirpApp.js
@@ -0,0 +1,7 @@
+// Angular app to control all the functionality of the app's front end
+// chirpApp.js
+var app = angular.module('chripApp', []);
+
+app.controller('mainController', function($scope){
+
+});
