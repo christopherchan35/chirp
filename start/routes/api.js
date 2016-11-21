07db50a36b067639e3bb284dbd01c65ba4d4af79
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-21 11:52:14 -0800

    add routes for a particular post

--- start/routes/api.js
+++ start/routes/api.js
@@ -16,4 +16,20 @@ router.route('/posts')
     res.send({message: 'TODO Create a new post'});
   });
 
+router.route('/posts/:id')
+  // return a particular post
+  .get(function(req, res){
+    res.send({message: 'TODO return post with ID ' + req.params.id});
+  })
+
+  // update existing post
+  .put(function(req, res){
+    res.send({message: 'TODO modify post with ID ' + req.params.id});
+  })
+
+  // delete existing post
+  .delete(function(req, res){
+    res.send({message: 'TODO delete post with ID ' + req.params.id});
+  })
+
 module.exports = router;
