f1d7d0ef0bcfdc12079ef47592982f218beccc15
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-21 21:26:47 -0800

    uncomment authenticate routes

--- start/app.js
+++ start/app.js
@@ -34,7 +34,7 @@ var initPassport = require('./passport-init');
 initPassport(passport);
 
 app.use('/api', api);
-// app.use('/auth', authenticate);
+app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
