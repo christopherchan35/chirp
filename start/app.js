7e17ce8851410c2aa2aa0f3500ec0f2f581a7368
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-11-21 17:40:56 -0800

    uncomment authenticate route

--- start/app.js
+++ start/app.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var api = require('./routes/api');
-// var authenticate = require('./routes/authenticate');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
